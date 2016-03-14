82881c1c1578ce3e5fd247a26db7375a7de137c2
dev216 <nasos.thomos216@gmail.com>
2016-03-14 21:42:28 +0200

    Code added to obtain the names of the columns for each table in the database. Of course the table sqlite_sequence is also added in this list.

--- main.py
+++ main.py
@@ -6,7 +6,36 @@ class PNavDb:
     pass
 
 
-db = sqlite3.connect("pnav.db")
+def GetTripIdList(db_conn_cursor):
+    db_conn_cursor.execute("SELECT tripId FROM trips")
+    result_set = db_conn_cursor.fetchall()
+    trip_id_set = set()
+    for resultset_tuple in result_set:
+        trip_id = resultset_tuple[0]
+        trip_id_set.add(trip_id)
+    trip_id_list = list()
+    if len(trip_id_set) > 1:
+        for trip_id in trip_id_set:
+            trip_id_list.append(trip_id)
+        trip_id_list.sort()
+    return trip_id_list if len(trip_id_list) > 1 else None
+
+
+def PrintTripPointsInfo(db_conn_cursor, trip_id):
+    db_conn_cursor.execute("SELECT latitude, longitude, unixtime, forceSave FROM trips WHERE tripId=:tripId",
+                           {"tripId": trip_id})
+    result_set = db_cursor.fetchall()
+    with open("trip_info_file", "a") as trip_info_file:
+        trip_info_file.write("====trip id #{0}====\n".format(trip_id))
+        for result_set_tuple in result_set:
+            trip_info_file.write("{0}\n".format(str(result_set_tuple)))
+            # unixtime = result_set_tuple[2]
+            # formatted_unixtime = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime(unixtime))
+
+
+# db = sqlite3.connect("pnav.db")
+db = sqlite3.connect("PNav3.db")
+
 db_cursor = db.cursor()
 db_cursor.execute('''SELECT name FROM sqlite_master
                      WHERE type IN ('table','view')
@@ -30,6 +59,7 @@ db_cursor.execute('''SELECT sql FROM
                      ORDER BY  tbl_name, type DESC, name''')
 db_info = db_cursor.fetchall()
 table_info_re = re.compile(r"^CREATE TABLE (\w+)(\s?)(\(.*\))")
+
 for a_table_info in db_info:
     tmp_table_info = a_table_info[0]
     table_info_matcher = table_info_re.match(tmp_table_info)
@@ -48,6 +78,11 @@ for a_table_info in db_info:
             table_column_parts = re.split(" ", a_table_column_descr.strip())
             table_column_name = table_column_parts[0]
             print(table_column_name)
+
+trip_id_list = GetTripIdList(db_cursor)
+# print(trip_id_list)
+for a_trip_id in trip_id_list:
+    PrintTripPointsInfo(db_cursor, a_trip_id)
 
 db_cursor.close()
 db.close()
